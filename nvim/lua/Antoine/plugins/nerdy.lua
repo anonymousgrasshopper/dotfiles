886726b15b1f997313fc4ce274094710556938eb
XiLambda <130611615+XiLambda@users.noreply.github.com>
2024-07-05 14:42:07 +0200

    added multiple fixes

--- nvim/lua/Antoine/plugins/nerdy.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "2kabhishek/nerdy.nvim",
-  lazy = true,
-    dependencies = {
-      'stevearc/dressing.nvim',
-      'nvim-telescope/telescope.nvim',
-    },
-  cmd = 'Nerdy',
-}
--- nvim/lua/Antoine/plugins/undotree.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "mbbill/undotree",
-  event = "VeryLazy",
-  priority = 75,
-  vim.keymap.set('n', '<leader>utree', vim.cmd.UndotreeToggle, { desc = "Toggle undotree" }) 
-}
-
