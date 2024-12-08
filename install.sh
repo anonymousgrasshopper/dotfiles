be0026c8c07cffc8dc5156ffe84e182ce80b2dd5
anonymousgrasshopper <antoine.s.27182@gmail.com>
2024-12-08 18:47:28 +0100

    add packages to install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo "export ZDOTDIR=\$HOME/.config/zsh" >> /etc/zsh/zshrc
 echo "zsh-newuser-install() { :; }" >> /etc/zsh/zshenv
 
 if [ -f /etc/arch-release ]
-  then sudo pacman -S eza fd fzf git github-cli man-db neovim npm python ripgrep tmux unzip wget xdotool zathura zathura-pdf-mupdf zoxide zsh
+  then sudo pacman -S bat eza fd fzf git github-cli man-db neovim npm python ranger ripgrep tmux unzip wget xdotool zathura zathura-pdf-mupdf zoxide zsh
 else
   echo "Make sure you install the packages listed at line 10 of install.sh"
 fi
