d02162f1f6958d113ff3d0b918adcec17e7bd0ca
anonymousgrasshopper <antoine.s.27182@gmail.com>
2024-12-14 11:31:14 +0100

    added missing shebang in compile_sfml and modified install.sh to move scripts in /usr/bin

--- install.sh
+++ install.sh
@@ -1,15 +1,22 @@
-if [ "$EUID" -ne 0 ]
-  then echo "Please run the script as root to proceed."
+if [ "$EUID" -ne 0 ] then
+  echo "Please run the script as root to proceed."
   exit
 fi
 
 echo "export ZDOTDIR=\$HOME/.config/zsh" >> /etc/zsh/zshrc
 echo "zsh-newuser-install() { :; }" >> /etc/zsh/zshenv
 
-if [ -f /etc/arch-release ]
-  then sudo pacman -S bat eza fd fzf git github-cli man-db neovim npm python ranger ripgrep tmux unzip wget xdotool zathura zathura-pdf-mupdf zoxide zsh
+if [ -f /etc/arch-release ] then
+  sudo pacman -S bat eza fd fzf git github-cli man-db neovim npm python ranger ripgrep tmux unzip wget xdotool zathura zathura-pdf-mupdf zoxide zsh
 else
   echo "Make sure you install the packages listed at line 10 of install.sh"
+fi
+
+if [ -f /usr/bin/compile_sfml ] then
+  sudo cp scripts/compile_sfml /usr/bin/compile_sfml
+fi
+if [ -f /usr/bin/codelldb_stdio_redirection ] then
+  sudo cp scripts/codelldb_stdio_redirection /usr/bin/codelldb_stdio_redirection
 fi
 
 echo "Follow instructions at https://www.tug.org/texlive/quickinstall.html to install TexLive"
