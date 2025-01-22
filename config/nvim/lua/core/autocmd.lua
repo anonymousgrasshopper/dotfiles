fe21125092c2da9f91f87eee82ae3410b50d99fd
anonymousgrasshopper <antoine.s.27182@gmail.com>
2025-01-22 17:46:39 +0100

    improve WSL installation instructions and add nvim keymap for marking words as correctly spelled

--- config/nvim/lua/core/autocmd.lua
+++ config/nvim/lua/core/autocmd.lua
@@ -128,5 +128,6 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.spell = true
 
     vim.keymap.set("i", "<C-l>", "<c-g>u<Esc>[s1z=`]a<c-g>u", { desc  = "Correct last spelling mistake", buffer = true })
+    vim.keymap.set("i", "<C-m>", "<c-g>u<Esc>[szg`]a<c-g>u", { desc  = "Add last word marked as misspelled to dictionnary", buffer = true })
   end,
 })
