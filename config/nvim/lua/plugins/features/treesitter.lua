7455d7a65f7311dea16befe7436a92169b07b15c
anonymousgrasshopper <antoine.s.27182@gmail.com>
2025-01-30 18:33:27 +0100

    fix weird behavior of nvim-treesitter-textobjects, modify some of its keymaps

--- config/nvim/lua/plugins/features/treesitter.lua
+++ config/nvim/lua/plugins/features/treesitter.lua
@@ -54,30 +54,31 @@ return {
               ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
               ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
             },
-            selection_modes = {
-              ["@parameter.outer"] = "v", -- charwise
-              ["@function.outer"] = "V", -- linewise
-              ["@class.outer"] = "V", -- linewise
-              ["@class.inner"] = "V", -- linewise
+            selection_modes = { -- "v" : charwise, "V" : linewise, "<c-v>" :blockwise
+              ["@parameter.outer"] = "v",
+              ["@function.outer"] = "V",
+              ["@class.outer"] = "V",
+              ["@class.inner"] = "v",
+              ["@local.scope"] = "V",
             },
-            include_surrounding_whitespace = true,
+            include_surrounding_whitespace = false, -- else @class.outer breaks
           },
           move = {
             enable = true,
             set_jumps = true, -- whether to set jumps in the jumplist
             goto_next_start = {
-              ["]m"] = "@function.outer",
+              ["]f"] = "@function.outer",
               ["]]"] = { query = "@class.outer", desc = "Next class start" },
               --
               -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queries.
-              ["]o"] = "@loop.*",
+              ["]l"] = "@loop.*",
               -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
               -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
               ["]s"] = { query = "@local.scope", query_group = "locals", desc = "Next scope" },
               ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
             },
             goto_next_end = {
-              ["]M"] = "@function.outer",
+              ["]F"] = "@function.outer",
               ["]["] = "@class.outer",
             },
             goto_previous_start = {
@@ -92,10 +93,10 @@ return {
             -- Use if you want more granular movements
             -- Make it even more gradual by adding multiple queries and regex.
             goto_next = {
-              ["]d"] = "@conditional.outer",
+              ["]c"] = "@conditional.outer",
             },
             goto_previous = {
-              ["[d"] = "@conditional.outer",
+              ["[c"] = "@conditional.outer",
             },
           },
         },
