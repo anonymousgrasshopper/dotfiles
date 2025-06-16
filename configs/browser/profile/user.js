64fca91d2ee377b52b04e3353b2684e6a5a3828d
anonymousgrasshopper <antoine.s.27182@gmail.com>
2025-06-16 16:51:04 +0200

    feat(browser): add user.js; feat(browser): greyscale dark mode for pdf.js

--- configs/browser/profile/user.js
+++ configs/browser/profile/user.js
@@ -1,5 +1,5 @@
-user.pref("toolkit.legacyUserProfileCustomizations.stylesheets") = true                     // enable userChrome.css
-user.pref("devtools.chrome.enabled") = true                                                 // enable browser console
-user.pref("devtools.debugger.remote-enabled") = true                                        // allow remote control
-user.pref("extension.pocket.enabled") = false                                               // disable Pocket
-user.pref("extensions.webextensions.addons-restricted-domains@mozilla.com.disabled") = true // allow addons to run in restricted domains
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);                     // enable userChrome.css
+user_pref("devtools.chrome.enabled", true);                                                 // enable browser console
+user_pref("devtools.debugger.remote-enabled", true);                                        // allow remote control
+user_pref("extension.pocket.enabled", false);                                               // disable Pocket
+user_pref("extensions.webextensions.addons-restricted-domains@mozilla.com.disabled", true); // allow addons to run in restricted domains
