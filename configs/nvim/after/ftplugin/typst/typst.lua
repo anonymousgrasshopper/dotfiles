53947791d45a2dcfce23a31ea91b9510d067808d
anonymousgrasshopper <antoine.s.27182@gmail.com>
2025-10-08 15:57:37 +0200

    fix(nvim): invalid makeprg for typst

--- configs/nvim/after/ftplugin/typst/typst.lua
+++ configs/nvim/after/ftplugin/typst/typst.lua
@@ -1,1 +1,1 @@
-vim.opt.makeprg = { "typst", "compile", "--diagnostic-format", "short" }
+vim.opt.makeprg = "typst compile --diagnostic-format short " .. vim.fn.shellescape(vim.api.nvim_buf_get_name(0))
